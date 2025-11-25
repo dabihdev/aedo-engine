74eef8b8b1fc84d0b6143df7737e42fd45a50d0c
dabihdev <dabihdev@gmail.com>
2025-11-25 16:45:12 +0100

    chore: added comments

--- src/run_game.py
+++ src/run_game.py
@@ -5,5 +5,6 @@ if __name__=="__main__":
     # Initialize game engine
     engine = GameEngine()
     
+    # Load game and run it
     if engine.load_game(GAMES_FOLDER+"my_adventure.json"):
         engine.run()
