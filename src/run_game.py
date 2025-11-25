31c584dab2415025f618d81c389c33242f00ba6c
dabihdev <dabihdev@gmail.com>
2025-11-25 16:44:42 +0100

    Feat: added template script to build games

--- src/run_game.py
+++ src/run_game.py
@@ -4,16 +4,6 @@ from settings import *
 if __name__=="__main__":
     # Initialize game engine
     engine = GameEngine()
-
-    # Load the game and run it
-    # FUTURE IMPLEMENTATION: add menu with following sections:
-    # > Game title
-    # > New Game
-    # > Load Game
-    # > Quit Game
-    # > user input
-    # Create a Menu class in the builder subpackage: the user should be able
-    # to create custom menus
     
     if engine.load_game(GAMES_FOLDER+"my_adventure.json"):
         engine.run()
---
+++ src/script_template.py
@@ -0,0 +1,22 @@
+from storywriter import GameBuilder, GameData, Room, Item, NPC, Event
+from settings import *
+
+# REPEAT FOR EACH LOCATION:
+
+# 1. Create enemies and npcs
+# ...
+
+# 2. Create items
+# ...
+
+# 3. Create events
+# ...
+
+# 4. Create location
+# ...
+# add exits
+# add enemies/npcs to location
+# add items to location
+# add events to location
+
+# 5. Add location to game data
